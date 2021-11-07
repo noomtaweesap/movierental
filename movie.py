59047f04814d01288631663ad28ad5c6942859e2
Taweesap Inliang <noom.taweesap@gmail.com>
2021-11-07 21:01:19 +0700

    Make Movie immutable and add attributes to Movie

--- movie.py
+++ movie.py
@@ -1,21 +1,36 @@
 """Movies information and details."""
+from typing import List
 
 
 class Movie:
     """A movie available for rent."""
     # The types of movies (price_code).
-    def __init__(self, title, price_code):
+    def __init__(self, title: str, price_code, year: int, genre: List[str]):
         # Initialize a new movie.
-        self.title = title
-        self.price_code = price_code
+        self._title = title
+        self._price_code = price_code
+        self._year = year
+        self._genre = genre
 
     def get_price_code(self):
-        """get the price code"""
-        return self.price_code
+        """get price code."""
+        return self._price_code
 
     def get_title(self):
-        """Get movie title."""
-        return self.title
+        """get movie title."""
+        return self._title
+
+    def get_year(self):
+        """get year."""
+        return self._year
+
+    def get_genre(self):
+        """get movie genre."""
+        return self._genre
+
+    def is_genre(self, genre: str):
+        """check movie genre."""
+        return genre in self._genre
 
     def __str__(self):
-        return self.title
+        return self._title
